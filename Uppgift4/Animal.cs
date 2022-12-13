3aa12a531c4404a7be94d72663f16c56dfc92b5f
Vincent <vinelb050125@student.jenseneducation.se>
2022-12-13 14:03:31 +0100

    Add project files.

---
+++ Uppgift4/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift4
+{ //Detta är en klass som vi kallar Animal
+    public class Animall
+    {   //Sätter ett startnamn och ett startår för djuret
+        private string Name = "jan";
+        private int Age = 0;
+        //Gör så att man kan sätta namn
+        public string getName()
+        {
+            return this.Name;
+        }
+        //Gör sp att man kan ändra namn
+        public void setName(string Name)
+        {
+            this.Name = Name;
+        }
+        //Gör så att man kan ge djuret ett år 
+        public int getAge()
+        {
+            return this.Age;
+        }
+        //Gör så att man kan ändra åldern
+        public void setAge(int Age)
+        {
+            this.Age = Age;
+        }
+        //Ger oss en klass i en klass som vi döper till dog
+        public class Dog : Animal
+        {
+            private int TailLength = 0;
+            //Ger hunden en svans
+            public int getTailLength()
+            {
+                return this.TailLength;
+            }
+            public void setTailLength(int TailLength)
+            {
+                this.TailLength = TailLength;
+            }
+        }
+    }
+}
+
+
+
+    
---
+++ Uppgift4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Uppgift4;
+
+
+
+Animal.Dog dog1 = new Animal.Dog();
+dog1.setName("björne");
+dog1.setAge(8);
+dog1.setTailLength(23);
+Console.WriteLine(dog1.getName());
+Console.WriteLine(dog1.getAge()+ "år gammal");
+Console.WriteLine(dog1.getTailLength()+"cm");
+Console.WriteLine("wruuf");
